60bc6a17019bd68581100a59d9e40ca97617242b
Robert Dumas <dumasrobert13@gmail.com>
2023-07-13 16:21:15 +0800

    SW-Dumas "finish the loginpage frontend"

--- front-end/lib/main.dart
+++ front-end/lib/main.dart
@@ -2,6 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:intern_mobile_app/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+
 
 void main() async {
   await Firebase.initializeApp(
@@ -19,6 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: GoogleFonts.poppinsTextTheme(),
         useMaterial3: true,
       ),
       home: const LoginPage(title: 'Flutter Demo Home Page'),
--- front-end/lib/pages/login_page.dart
+++ front-end/lib/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intern_mobile_app/components/login_button.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key, required String title});
@@ -26,25 +28,68 @@ class LoginPage extends StatelessWidget {
                   stops: [0.3, 1.0],
                   colors: [
                     Color.fromARGB(240, 29, 11, 42),
-                    Color.fromARGB(37, 15, 54, 0),
+                    Color.fromARGB(0, 37, 15, 54),
                   ],
                 ),
               ),
             ),
             Center(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  //logo
-                  Image.asset(
-                    'lib/assets/images/log-in/logo.png',
+                  //logo and subtitle
+                  SvgPicture.asset(
+                    'lib/assets/images/log-in/logo.svg',
                     height: 168,
                     width: 209,
                   ),
-                  //create an account or log in
-
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Find a Romance',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
                   //continue with email
-
+                  const SizedBox(height: 66),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 21.0),
+                    child: Column(
+                      children: [
+                        const Text(
+                          'Create an account or login',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        //button for google
+                        LoginButton(
+                            text: 'Continue with Google', onPressed: () {}, logo: 'lib/assets/images/log-in/google.png',),
+                        const SizedBox(height: 10),
+                        //button for facebook
+                        LoginButton(
+                            text: 'Continue with Facebook', onPressed: () {}, logo: 'lib/assets/images/log-in/facebook.png',),
+                        const SizedBox(height: 10),
+                        //button for email
+                        LoginButton(
+                            text: 'Continue with Email', onPressed: () {}, logo: 'lib/assets/images/log-in/email.png',),
+                        const SizedBox(height: 29),
+                        const Text(
+                          'By signing up, you agree to our Terms of Service and Privacy Policy',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10,
+                          ),
+                        ),
+                        const SizedBox(height: 47),
+                      ],
+                    ),
+                  ),
                   //terms of service
                 ],
               ),
